7609ba5ac49cd2742e7a4a53ca631ef5c2665155
Hemant Singh Bisht <bishthemant96@gmail.com>
2020-06-07 23:43:58 +0530

    Added responsiveness to navigation bar.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,33 @@
 export default {
-  name: 'Header'
+  name: 'Header',
+
+  data(){
+    return {
+      isToggled: false,
+    }
+  },
+
+  methods: {
+    toggleNavigation: function(){
+      if(!this.isToggled){
+        document.getElementById("title-container").style.order = '1';
+        document.getElementById("nav-toggle").style.order = '2';
+        document.getElementById("nav-container").style.order = '3';
+        document.getElementById("nav-container").style.width = '100%';
+        document.getElementById("nav-container").style.display = 'flex';
+        document.getElementsByClassName("head-container")[0].style.flexWrap = "wrap";
+      } else {
+        document.getElementById("title-container").style.order = '1';
+        document.getElementById("nav-toggle").style.order = '2';
+        document.getElementById("nav-container").style.order = '3';
+        document.getElementById("nav-container").style.width = '100%';
+        document.getElementById("nav-container").style.display = 'none';
+        document.getElementsByClassName("head-container")[0].style.flexWrap = "wrap";
+      }
+
+      
+      this.isToggled = !this.isToggled;
+      
+    }
+  }
 }
