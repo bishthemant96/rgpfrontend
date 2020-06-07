a93438f07d097f0075e043876e0dc1fc8b389f6d
Hemant Singh Bisht <bishthemant96@gmail.com>
2020-06-07 13:34:10 +0530

    Added a footer on home page.

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,10 +1,12 @@
 import Header from "../../components/header/Header.vue";
 import Blogpost from "../../components/blogpost/Blogpost.vue";
+import Footer from "../../components/footer/Footer.vue";
 
 export default {
     name: 'Home',
     components: {
       Header,
-      Blogpost
+      Blogpost,
+      Footer
     }
   }
