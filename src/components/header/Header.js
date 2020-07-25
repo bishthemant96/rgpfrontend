fc22bb2e0fdf21ef8ee2dd0a151039567f2d4c27
Hemant Singh Bisht <bishthemant96@gmail.com>
2020-07-26 02:33:25 +0530

    Temporarily removed responsive design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,31 +1,3 @@
 export default {
-  name: 'Header',
-
-  data(){
-    return {
-      isToggled: false,
-    }
-  },
-
-  methods: {
-    toggleNavigation: function(){
-      if(!this.isToggled){
-        document.getElementById("title-container").style.order = '1';
-        document.getElementById("nav-toggle").style.order = '2';
-        document.getElementById("nav-container").style.order = '3';
-        document.getElementById("nav-container").style.width = '100%';
-        document.getElementById("nav-container").style.display = 'flex';
-        document.getElementsByClassName("head-container")[0].style.flexWrap = "wrap";
-      } else {
-        document.getElementById("title-container").style.order = '1';
-        document.getElementById("nav-toggle").style.order = '2';
-        document.getElementById("nav-container").style.order = '3';
-        document.getElementById("nav-container").style.width = '100%';
-        document.getElementById("nav-container").style.display = 'none';
-        document.getElementsByClassName("head-container")[0].style.flexWrap = "wrap";
-      }
-      
-      this.isToggled = !this.isToggled;
-    }
-  }
+  name: 'Header'
 }
