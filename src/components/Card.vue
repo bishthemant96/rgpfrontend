ec2e9d9b6052389bf2ebad76373de5c0ecf4db70
Hemant Singh Bisht <bishthemant96@gmail.com>
2020-07-28 23:07:57 +0530

    Small bug fixed from last commit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <v-card max-width="330">
-        <v-img height="200px" :src="require('../assets/banner.jpg')"/>
+        <v-img height="200px" :src="require('../assets/university.jpeg')"/>
         <v-card-title>Care Taker Unit</v-card-title>
         <v-card-subtitle> Temple Tree Retreat </v-card-subtitle>
     </v-card>
