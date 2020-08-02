8e6f90fc90a5a80efbd2e1a0664a0f841d78549a
Hemant Singh Bisht <bishthemant96@gmail.com>
2020-08-02 13:04:10 +0530

    card aesthetics improvement

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-card class="pt-2 card-style" @click="onClick(data['project-number'])" tile>
-        <v-img height="175px" :src="require('../assets/project-' + data['project-number'] + '-1.jpg')"/>
-        <v-card-title> {{ data["project-name"].split(',')[0] }} </v-card-title>
-        <v-card-subtitle> {{ data["project-name"].split(',')[1] }} </v-card-subtitle>
+    <v-card class="pt-6 px-3 card-style" @click="onClick(data['project-number'])" tile>
+        <v-img height="220px" :src="require('../assets/project-' + data['project-number'] + '-1.jpg')"/>
+        <v-card-title class="px-0 py-2 text-center"> {{ data["project-name"].split(',')[0] }} </v-card-title>
+        <v-card-subtitle class="px-0"> {{ data["project-name"].split(',')[1] }} </v-card-subtitle>
     </v-card>
 </template>
 
